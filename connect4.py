ae6715a1a8dfc0a9d51130460801a6b9e34b73d4
Sean Welsh <s.welsh1314@gmail.com>
2022-04-28 16:18:12 -0400

    can now access each tile individually and change as needed

--- connect4.py
+++ connect4.py
@@ -12,8 +12,11 @@ class Squares:
         self.row = ('| ' + self.chip + ' ')
         print(self.row, end='')
     
-    def change_chip(self):
-        self.chip = 'x'
+    def change_chip(self, player):
+        if player == player1:
+            self.chip = 'x'
+        if player == player2:
+            self.chip = 'o'
 
 
 class Grid:
@@ -23,13 +26,13 @@ class Grid:
         for row in range(7):
             self.square.append([])
             for column in range(7):
-                grid_point1 = Squares('o')
+                grid_point1 = Squares(' ')
                 self.square[row].append(grid_point1)
-
+#prints grid each square is a point on the grid in square[column][row] format starting at the top left[0][0] square to bottom right[6][5]
     def grid(self):
-        self.square[0][0].change_chip()
-        self.square[0][1].change_chip()
-        self.square[0][2].change_chip()
+        # self.square[0][0].change_chip()
+        # self.square[0][1].change_chip()
+        # self.square[0][2].change_chip()
         # self.square[0][3].change_chip()
         # self.square[0][4].change_chip()
         # self.square[0][5].change_chip()
@@ -94,8 +97,28 @@ class Grid:
             if grid == 5:
                 print()
                 print('+---' *7 + '+')
-
+    def populate_square(self, user_input, player):
+        x=5
+        for row in range(6):
+            if self.square[user_input][x].chip ==' ':
+                self.square[user_input][x].change_chip(player)
+                break
+            x-=1
+print('Welcome to connect4!')
+player1 = input('Player 1 please enter your name ')
+player2 = input('Player 2 please enter your name ')
+print(player1, ' you will be in control of the x tokens ', player2, ' you will be in control of the o tokens here is your blank board. Good luck!!\n\n')
 game = Grid()
 game.grid()
 # first_square = Squares()
 # second_square = Squares('x')
+i_say_so= 'n'
+while i_say_so == 'n':
+    player1_move = input('\n\n' + player1 + ' please choose the column you would like to place your token in ')
+    game.populate_square(int(player1_move), player1)
+    game.grid()
+    player2_move = input('\n\n' + player2 + ' please choose the column you would like to place your token in ')
+    game.populate_square(int(player2_move), player2)
+    game.grid()
+    i_say_so = input('is the game over ')
+
